7aa5a2e2ccb7ce2e2b6105eef8a3580a55b3bec7
Shiun <purpleclouds0916@gmail.com>
2021-01-05 16:04:36 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   root 'static_pages#home'
   get  '/og',    to: 'static_pages#og'
+  get  '/signup',  to: 'users#new'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
