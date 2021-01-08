bf5fe73db8dbb7660b0db5f44829ad6c6e4383e1
Shiun <purpleclouds0916@gmail.com>
2021-01-08 18:58:48 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  
-  get 'alumni_messages/message'
   root 'static_pages#home'
   get  '/message',    to: 'alumni_messages#message'
   get  '/signup',  to: 'users#new'
+  get 'alumni_messages/message'
+  resources :users
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
