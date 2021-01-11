6eeb7a4862ab0379166fb88e2945843f8febdd4c
Shiun <purpleclouds0916@gmail.com>
2021-01-11 12:41:42 +0900

    Request the right user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user
+  before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -43,4 +45,16 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end  
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+  end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "invalid signup information" do
+    log_in_as(@user)
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "",
@@ -16,6 +21,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
 
   test "valid signup information" do
+    log_in_as(@user)
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
