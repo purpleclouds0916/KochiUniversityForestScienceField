ac362c45355b89e6e8231a13e0337074819a153a
Shiun <purpleclouds0916@gmail.com>
2021-01-14 14:48:47 +0900

    Remove the ability to log in as a user when creating a user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
       flash[:success] = "新しい管理者を作成しました。"
       redirect_to users_path
     else
