618761a202a3211a0611202d6eff4fe7d8036bb3
Shiun <purpleclouds0916@gmail.com>
2021-01-18 13:48:32 +0900

    Resolved validation errors in contact form

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -7,10 +7,14 @@ class InquiryController < ApplicationController
 
   def thanks
     # メール送信
-    @inquiry = Inquiry.new(params[:inquiry].permit(:name, :email, :message))    
-    InquiryMailer.received_email(@inquiry).deliver
-
-    # 完了画面を表示
-    render :action => 'thanks'
+    @inquiry = Inquiry.new(params[:inquiry].permit(:name, :kinds, :email, :message))  
+    if @inquiry.valid?
+      # OK。確認画面を表示
+      render :action => 'thanks'
+      InquiryMailer.received_email(@inquiry).deliver
+    else
+      # NG。入力画面を再表示
+      render :action => 'index'
+    end  
   end
 end
--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -5,4 +5,5 @@ class Inquiry < ApplicationRecord
     validates :email, presence: true, length: { maximum: 255 },
                 format: { with: VALID_EMAIL_REGEX },
                 uniqueness: true
+    validates :message, presence: true             
 end
