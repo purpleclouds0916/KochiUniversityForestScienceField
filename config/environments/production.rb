f5d65cd3a8b7fd74373f797becdcb45c3a697634
Shiun <purpleclouds0916@gmail.com>
2021-01-19 18:40:45 +0900

    change something

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,7 +113,7 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
   config.force_ssl = true
-
+  config.action_mailer.raise_delivery_errors = false
   #
   config.action_mailer.default_url_options = {  :host => 'https://ltqphygm.herokuapp.com/' }
 
