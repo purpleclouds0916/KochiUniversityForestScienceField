15687dbd3d89889e480f53009f8b804b40ab32ad
Shiun <purpleclouds0916@gmail.com>
2021-01-19 13:11:57 +0900

    Change the email address to be sent to

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -3,6 +3,6 @@ class InquiryMailer < ApplicationMailer
 
   def received_email(inquiry)
     @inquiry = inquiry
-    mail(:to => inquiry.email, :subject => 'お問い合わせを承りました')
+    mail(:to => "purpleclouds0916@gmail.com", :subject => 'お問い合わせを承りました')
   end
 end
