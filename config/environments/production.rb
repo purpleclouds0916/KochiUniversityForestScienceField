d326d9f82c49b3479280bd0b211e9d2aa99af4f3
Shiun <purpleclouds0916@gmail.com>
2021-01-19 18:37:33 +0900

    change something

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -123,8 +123,8 @@ Rails.application.configure do
     address:"smtp.gmail.com",
     domain: 'gmail.com',
     port:587,
-    user_name: ENV['SEND_MAIL'],
-    password: ENV['SEND_MAIL_PASSWORD'],
+    user_name:            ENV['SEND_MAIL'],
+    password:             ENV['SEND_MAIL_PASSWORD'],
     authentication: :login
   }
 end
