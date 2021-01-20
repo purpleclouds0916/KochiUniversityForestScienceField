3cebe2ae8b73aeb6eaac4f072458e2bf5bb1e43c
Shiun <purpleclouds0916@gmail.com>
2021-01-20 14:42:33 +0900

    delete a mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -129,18 +129,4 @@ Rails.application.configure do
       password:Rails.application.credentials.gmail[:password],
       authentication: :login
     }
-    
-
-    # config.action_mailer.raise_delivery_errors = true
-    # config.action_mailer.delivery_method = :smtp
-    # host = 'https://ltqphygm.herokuapp.com/'
-    # config.action_mailer.default_url_options = { host: host }
-    # ActionMailer::Base.smtp_settings = {
-    #   :port           => ENV['MAILGUN_SMTP_PORT'],
-    #   :address        => ENV['MAILGUN_SMTP_SERVER'],
-    #   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    #   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    #   :domain         => host,
-    #   :authentication => :plain,
-    # }
 end
