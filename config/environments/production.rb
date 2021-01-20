1b893ea239ae4767afc85902589fe8bd8bbdeb08
Shiun <purpleclouds0916@gmail.com>
2021-01-20 11:17:18 +0900

    change something

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,12 +120,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
     #Action Mailerのためにgmailの登録
-    config.action_mailer.smtp_settings = {
-      address:"smtp.gmail.com",
-      domain: 'gmail.com',
-      port:587,
-      user_name:Rails.application.credentials.gmail[:user_name],
-      password:Rails.application.credentials.gmail[:password],
-      authentication: :login
-    }
+    # config.action_mailer.smtp_settings = {
+    #   address:"smtp.gmail.com",
+    #   domain: 'gmail.com',
+    #   port:587,
+    #   user_name:Rails.application.credentials.gmail[:user_name],
+    #   password:Rails.application.credentials.gmail[:password],
+    #   authentication: :login
+    # }
 end
