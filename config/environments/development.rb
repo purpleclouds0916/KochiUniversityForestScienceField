4d0747d5455095347f06343f9401335e48756db0
Shiun <purpleclouds0916@gmail.com>
2021-01-21 08:52:22 +0900

    Comment out the gmail settings in the development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,19 +60,19 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   
   config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
 
   #Action Mailerのためにgmailの登録
-  config.action_mailer.smtp_settings = {
-    address:"smtp.gmail.com",
-    domain: 'gmail.com',
-    port:587,
-    user_name:Rails.application.credentials.gmail[:user_name],
-    password:Rails.application.credentials.gmail[:password],
-    authentication: :login
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address:"smtp.gmail.com",
+  #   domain: 'gmail.com',
+  #   port:587,
+  #   user_name:Rails.application.credentials.gmail[:user_name],
+  #   password:Rails.application.credentials.gmail[:password],
+  #   authentication: :login
+  # }
 end
