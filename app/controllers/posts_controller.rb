fe76277cc381a88b63f810f19b5e577cd59d6ccc
Shiun <purpleclouds0916@gmail.com>
2021-01-21 12:10:58 +0900

    Add deletion of user's post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,10 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = current_user.posts.build
-    # @post = current_user.posts.build if logged_in?
+    if logged_in?
+      @post = current_user.posts.build
+      #@feed_items = current_user.feed.paginate(page: params[:page])
+    end  
   end
 
   def create
@@ -15,11 +17,28 @@ class PostsController < ApplicationController
       flash[:success] = "post created!"
       redirect_to current_user
     else
-      render 'static_pages/home'
+      # @feed_items = current_user.feed.paginate(page: params[:page])
+    render :action => 'new'
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @user = @post.user
+    @post.destroy
+    flash[:success] = "投稿を削除しました。"
+    redirect_to user_path(@user)
+  end
+
+
+  private
+
   def post_params
     params.require(:post).permit(:content, :title)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 end
