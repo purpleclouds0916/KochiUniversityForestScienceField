53178176e77e54b062418e9bb8d4f3dccf6d9bbc
Shiun <purpleclouds0916@gmail.com>
2021-01-21 09:07:13 +0900

    add post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :microposts
     attr_accessor :remember_token, :reset_token
     before_save { email.downcase! }
     validates :name,  presence: true, length: { maximum: 50 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_17_054818) do
+ActiveRecord::Schema.define(version: 2021_01_21_000203) do
 
   create_table "inquiries", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 2021_01_17_054818) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
     t.string "kinds"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +44,5 @@ ActiveRecord::Schema.define(version: 2021_01_17_054818) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
