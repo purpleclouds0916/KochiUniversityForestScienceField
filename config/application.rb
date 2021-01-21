5090797c91a830d3e24b7ff2afb84864eb6d9bf8
Shiun <purpleclouds0916@gmail.com>
2021-01-21 16:42:07 +0900

    Fixing the view of the post list

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module HpApp
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
   end
 end
