2febfee7d01794fb1d29b1b10d252d8b82d2ec12
Shiun <purpleclouds0916@gmail.com>
2021-01-21 18:19:42 +0900

    Change the redirection destination at login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or users_path
+      redirect_back_or user
     else
       flash.now[:danger] = 'メールアドレスまたはパスワードが間違えています。' 
       render 'new'
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -10,11 +10,11 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
-    assert_redirected_to users_path
+    assert_redirected_to user_path(@user)
     follow_redirect!
-    assert_template 'users/index'
+    assert_template 'users/show'
     assert_select "div.contact__control", count: 0
-    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", edit_user_path(@user)
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
