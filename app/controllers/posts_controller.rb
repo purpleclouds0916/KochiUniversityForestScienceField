6c0410cdfce30bf4d7dd02e3426f4c9a354cf63d
Shiun <purpleclouds0916@gmail.com>
2021-01-21 18:54:25 +0900

    Adding article edits

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,13 +16,28 @@ class PostsController < ApplicationController
     @post = current_user.posts.build(post_params)
     @post.images.attach(params[:post][:images])
     if @post.save
-      flash[:success] = "post created!"
+      flash[:success] = "投稿を作成しました"
       redirect_to current_user
     else
       # @feed_items = current_user.feed.paginate(page: params[:page])
     render :action => 'new'
     end
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    if @post.update(post_params)
+      flash[:success] = "投稿を更新しました"
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
+end
 
   def destroy
     @post = Post.find(params[:id])
