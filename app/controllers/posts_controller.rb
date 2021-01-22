04e74ac5774ea77f68c8f5c6c7e752d288534e36
Shiun <purpleclouds0916@gmail.com>
2021-01-22 10:09:35 +0900

    Fixed so that users can't post without logging in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,7 @@
 class PostsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :logged_in_user
 
   def index
- 
     @posts = Post.page(params[:page]).per(10)
   end
 
