39588d30321c360c582aaa2e3f25838aee068a1f
Shiun <purpleclouds0916@gmail.com>
2021-01-23 10:32:44 +0900

    Improved database loading speed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :logged_in_user
 
   def index
-    @posts = params[:tag_id].present? ? Tag.find(params[:tag_id]).posts : Post.all
+    @posts = params[:tag_id].present? ? Tag.find(params[:tag_id]).posts : Post
     @posts = @posts.page(params[:page]).per(10)
   end
 
