cdca1ec5d643680f6f9e167e92b2f2475fe0672c
Shiun <purpleclouds0916@gmail.com>
2021-01-26 22:00:02 +0900

    add js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,10 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("jquery")
+//リッチエディタ
+require("trix")
+require("@rails/actiontext")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -39,5 +42,3 @@ window.onload = () => {
     })
   
   }
-require("trix")
-require("@rails/actiontext")
