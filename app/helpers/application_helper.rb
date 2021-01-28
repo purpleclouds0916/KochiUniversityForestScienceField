08f44834e57586adf0331802b5c628c479be1b70
ShiunMurata <purpleclouds0916@gmail.com>
2021-01-28 13:01:40 +0900

    add title and delete create.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    
+  # ページごとの完全なタイトルを返す                  
+  def full_title(page_title = '')                     
+    base_title = "高知大学 森林科学領域 公式ホームページ"  
+    if page_title.empty?                              
+      base_title                                      
+    else
+      page_title + " | " + base_title                 
+    end
+  end
 end
