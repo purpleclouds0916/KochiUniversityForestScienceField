9ddab35cba115a41a729f224e1c7f7713c886e7b
ShiunMurata <purpleclouds0916@gmail.com>
2021-01-28 12:29:42 +0900

    Organize js files

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -312,17 +312,67 @@ $(function(){
     });
   });
 
+ //以下管理者画面のjs
+ // -----
+// 投稿タグとフォームの出しわけ
+// -----
 
-  //画面サイズの変更でリロードして、デザインが崩れるのを防止
   $(function(){
-    var timer = false;
-    $(window).resize(function() {
-      if (timer !== false) {
-      clearTimeout(timer);
+
+    var switchBtn = $('input[name=type]');
+    var switchItems = $('.switchItems');
+    var inputField = 'input, textarea';
+  
+    switchBtn.on('change', function(){
+    
+      var inputValue = $(this).val();
+      
+    function buildItems() {
+      switchItems.show();
+          switchItems.find(inputField).each(function(){
+            $(this).prop('disabled', false);
+          });
+          hideAndDisabledItems.hide();
+          hideAndDisabledItems.find(inputField).each(function(){
+            $(this).prop('disabled', true);
+          });
+    }
+  
+      if(inputValue === 'スライダー'){
+        var hideAndDisabledItems = $('.switch_movie, .switch_link, .switch_og');
+        buildItems();
+      }else if(inputValue === '森林を学ぶ価値'){
+        var hideAndDisabledItems = $('.switch_movie, .switch_link, .switch_og, .switch_sb_title');
+        buildItems();
+      }else if(inputValue === '森林科学領域'){ 
+        var hideAndDisabledItems = $('.switch_link, .switch_og, .switch_sb_title');
+        buildItems();
+      }else if(inputValue === '先生の紹介'){
+        var hideAndDisabledItems = $('.switch_og');
+        buildItems();
+      }else if(inputValue === '授業の紹介'){ 
+        var hideAndDisabledItems = $('.switch_link, .switch_og, .switch_sb_title');
+        buildItems();
+      }else if(inputValue === '卒業生の声'){
+        var hideAndDisabledItems = $('.switch_movie, .switch_title');
+        buildItems();
+      }else if(inputValue === '資格'){ 
+        var hideAndDisabledItems = $('.switch_og, .switch_sb_title, .switch_movie');
+        buildItems();
       }
-      timer = setTimeout(function() {
-      //リロードする
-      location.reload();
-      }, 200);
     });
   });
+  
+    //画面サイズの変更でリロードして、デザインが崩れるのを防止
+    $(function(){
+      var timer = false;
+      $(window).resize(function() {
+        if (timer !== false) {
+        clearTimeout(timer);
+        }
+        timer = setTimeout(function() {
+        //リロードする
+        location.reload();
+        }, 200);
+      });
+    });
