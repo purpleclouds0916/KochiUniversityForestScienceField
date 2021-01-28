d5a2c17d89c048c6897570c030e4528a0734f16a
ShiunMurata <purpleclouds0916@gmail.com>
2021-01-28 12:36:58 +0900

    Organize js files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,13 +15,6 @@ const swiper = new Swiper
 import Swiper from 'swiper/bundle';
 window.Swiper = Swiper
 
-
-  // import Swiper JS
-
-
-// import WOW from 'wow.js'
-// window.WOW = WOW
-
 require("bootstrap")
 import "../stylesheets/application";
 document.addEventListener("turbolinks:load", function() {
@@ -36,40 +29,3 @@ const WOW = require('wowjs');
 window.wow = new WOW.WOW({
   live: false
 });
-
-
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
-
-// window.onload = () => {
-
-//     class Confirm {
-//       constructor(el) {
-//         this.message = el.getAttribute('data-confirm')
-//         if (this.message) {
-//           el.form.addEventListener('submit', this.confirm.bind(this))
-//         } else {
-//           console && console.warn('No value specified in `data-confirm`', el)
-//         }
-//       }
-  
-//       confirm(e) {
-//         if (!window.confirm(this.message)) {
-//           e.preventDefault();
-//         }
-//       }
-//     }
-  
-//     Array.from(document.querySelectorAll('[data-confirm]')).forEach((el) => {
-//       new Confirm(el)
-//     })
-  
-//   }
-
--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -92,7 +92,10 @@ window.wow.init();
 //   $("#post_content_trix_input_post").addClass("switch_content");
 // })
 
-//フォームのタグによりフォームの出しわけ
+ //以下管理者画面のjs
+ // -----
+// 投稿タグとフォームの出しわけ
+// -----
 $(function(){
 
     var switchBtn = $('input[type=radio]');
@@ -161,57 +164,6 @@ $(function(){
     });
   });
 
- //以下管理者画面のjs
- // -----
-// 投稿タグとフォームの出しわけ
-// -----
-
-  $(function(){
-
-    var switchBtn = $('input[name=type]');
-    var switchItems = $('.switchItems');
-    var inputField = 'input, textarea';
-  
-    switchBtn.on('change', function(){
-    
-      var inputValue = $(this).val();
-      
-    function buildItems() {
-      switchItems.show();
-          switchItems.find(inputField).each(function(){
-            $(this).prop('disabled', false);
-          });
-          hideAndDisabledItems.hide();
-          hideAndDisabledItems.find(inputField).each(function(){
-            $(this).prop('disabled', true);
-          });
-    }
-  
-      if(inputValue === 'スライダー'){
-        var hideAndDisabledItems = $('.switch_movie, .switch_link, .switch_og');
-        buildItems();
-      }else if(inputValue === '森林を学ぶ価値'){
-        var hideAndDisabledItems = $('.switch_movie, .switch_link, .switch_og, .switch_sb_title');
-        buildItems();
-      }else if(inputValue === '森林科学領域'){ 
-        var hideAndDisabledItems = $('.switch_link, .switch_og, .switch_sb_title');
-        buildItems();
-      }else if(inputValue === '先生の紹介'){
-        var hideAndDisabledItems = $('.switch_og');
-        buildItems();
-      }else if(inputValue === '授業の紹介'){ 
-        var hideAndDisabledItems = $('.switch_link, .switch_og, .switch_sb_title');
-        buildItems();
-      }else if(inputValue === '卒業生の声'){
-        var hideAndDisabledItems = $('.switch_movie, .switch_title');
-        buildItems();
-      }else if(inputValue === '資格'){ 
-        var hideAndDisabledItems = $('.switch_og, .switch_sb_title, .switch_movie');
-        buildItems();
-      }
-    });
-  });
-  
     //画面サイズの変更でリロードして、デザインが崩れるのを防止
     $(function(){
       var timer = false;
