f7bcfb70d1f192e9934a7f4c0d8998c05aa99864
ShiunMurata <purpleclouds0916@gmail.com>
2021-01-30 19:49:30 +0900

    Modified to fix the problem that references are not displayed and the design is broken

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,11 +4,14 @@ class StaticPagesController < ApplicationController
   def home
     @alumnus = Tag.find(6).posts.limit(4)
     @lessons = Tag.find(5).posts.limit(1)
-    @meaning = Tag.find(2).posts.limit(3)
+    @meanings = Tag.find(2).posts.limit(3)
     @about = Tag.find(3).posts.limit(1)
     @skills  = Tag.find(7).posts.limit(8)
     @teacher = Tag.find(4).posts.limit(6)
     @teacher_video = Tag.find(8).posts
+    @external_urls = @meanings.each do |meaning|
+       puts meaning.external_url
+   end
   end
 
   def alumni
