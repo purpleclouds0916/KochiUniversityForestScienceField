d10c449e9f0d1a5cf8f90f95abf95f8732bc3236
ShiunMurata <purpleclouds0916@gmail.com>
2021-01-30 13:02:53 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,41 +15,6 @@ User.create!(name:  "Example User",
                password_confirmation: password)
 end
 
-users = User.order(:created_at).take(6)
-# 10.times do
-#   content = Faker::Lorem.sentence(word_count: 20)
-#   aa = Faker::Address.city
-#   job = Faker::Job
-#   users.each { |user| user.posts.create!(content: content, birthplace: aa, job:aa, job_description: aa,learning:aa,memories:aa,name:aa,original_content:aa,original_title:aa,reason:aa,research_field:aa,research_office:aa) }
-# end
-10.times do
-  users.each do |user|
-    name  = Faker::Name.name
-    sentence1 = Faker::Lorem.sentence(word_count: 2)
-    sentence2 = Faker::Lorem.sentence(word_count: 3)
-    sentence3 = Faker::Lorem.sentence(word_count: 8)
-    sentence4 = Faker::Lorem.sentence(word_count: 100)
-    sentence5 = Faker::Lorem.sentence(word_count: 100)
-
-    birthplace = Faker::Address.city
-    job = Faker::Job
-    
-    user.posts.create!(
-      content: sentence1, 
-      birthplace: birthplace, 
-      job:sentence1,
-      job_description: sentence5,
-      learning:sentence5,memories:sentence5,name:name,original_content:sentence5,original_title:sentence3,
-      reason:sentence5,research_field:sentence1,research_office:sentence1
-    )
-  end
-end
-
-
-
-
-# , job:sentence, job_description: aa,learning:aa,memories:aa,name:aa,original_content:aa,original_title:aa,reason:aa,research_field:aa,research_office:aa 
-# 追加
 Tag.create([
   { name: 'スライダー' },
   { name: '森林を学ぶ価値' },
@@ -61,12 +26,47 @@ Tag.create([
   { name: '先生の紹介動画' }
 ])
 
+users = User.order(:created_at).take(6)
+# 10.times do
+#   content = Faker::Lorem.sentence(word_count: 20)
+#   aa = Faker::Address.city
+#   job = Faker::Job
+#   users.each { |user| user.posts.create!(content: content, birthplace: aa, job:aa, job_description: aa,learning:aa,memories:aa,name:aa,original_content:aa,original_title:aa,reason:aa,research_field:aa,research_office:aa) }
+# end
+10.times do
+  users.each do |user|
+    name  = Faker::Name.name
+    birthplace = Faker::Address.city
+    job = Faker::Company.name
+    sentence1 = Faker::Lorem.sentence(word_count: 2)
+    sentence2 = Faker::Lorem.sentence(word_count: 3)
+    sentence3 = Faker::Lorem.sentence(word_count: 8)
+    sentence4 = Faker::Lorem.sentence(word_count: 100)
+    sentence5 = Faker::Lorem.sentence(word_count: 100)
+    sentence6 = Faker::Lorem.sentence(word_count: 100)
+    sentence7 = Faker::Lorem.sentence(word_count: 100)
+    sentence8 = Faker::Lorem.sentence(word_count: 100)
+    
+    user.posts.create!(tag_ids: '6',
+      name:name, birthplace: birthplace, job:job, research_field:sentence1, research_office:sentence2,
+      reason:sentence4, learning:sentence5, job_description: sentence6, memories:sentence7, original_title:sentence3, original_content:sentence8
+    )
+    # post.tags << Tag.find_by({name:"卒業生の声"})
+    # filename = filenames[3..22].shuffle.first
+    #  file_path = "app/assets/images/samples/#{filename}"
+    #   post.images.attach(io: File.open("#{file_path}"),
+    # filename: "samples/#{filename}")
+  end
+end
+
+
+
 # > user.posts.first.tags.first
 posts = Post.all
 filenames = Dir.open("app/assets/images/samples/",&:to_a)
 
 posts.each do |post|
-  post.tags << Tag.find_by({name:"卒業生の声"})
+  # post.tags << Tag.find_by({name:"卒業生の声"})
 filename = filenames[3..22].shuffle.first
  file_path = "app/assets/images/samples/#{filename}"
   post.images.attach(io: File.open("#{file_path}"),
