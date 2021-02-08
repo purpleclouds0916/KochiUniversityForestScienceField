5040230d7cbec1eaca8b7544d089dc953a5ad66d
ShiunMurata <purpleclouds0916@gmail.com>
2021-02-08 21:21:47 +0900

    seedようの写真の追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,7 +41,7 @@ filenames = Dir.open("db/fixtures/",&:to_a)
     sentence6 = Faker::Lorem.sentence(word_count: 100)
     sentence7 = Faker::Lorem.sentence(word_count: 100)
     sentence8 = Faker::Lorem.sentence(word_count: 100)
-    filename = filenames[5..32].shuffle.first
+    filename = filenames[4..69].shuffle.first
  file_path = "db/fixtures/#{filename}"
 
 #  post = user.posts.create!(tag_id: '6')
