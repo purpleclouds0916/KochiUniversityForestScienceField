e4ece0321583c13a831193f2a4ba973f9ad1b6ad
ShiunMurata <purpleclouds0916@gmail.com>
2021-02-13 15:27:07 +0900

    フレンドリーフォワーディングのコメントアウトを元にもだした

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      # redirect_back_or posts_path
-      redirect_to posts_path
+      redirect_back_or posts_path
     else
       flash.now[:danger] = 'メールアドレスまたはパスワードが間違えています。' 
       render 'new'
